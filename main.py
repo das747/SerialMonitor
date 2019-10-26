c8bf103631645f20447e950ddd2234b38529c850
das747 <40293014+das747@users.noreply.github.com>
2019-10-26 19:23:40 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from design import Ui_MainWindow
+import sys
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    sys.exit(app.exec())
